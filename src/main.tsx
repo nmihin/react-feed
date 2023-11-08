d4d09ad9bac4435873a0559b2096d99d39a8c85e
Nikola Mihin <nmihin@gmail.com>
2023-11-08 13:36:50 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/react-feed/"}>
       <QueryProvider>
         <AuthProvider>
           <App />
