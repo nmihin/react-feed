649682d1e754cd743ba61c664be0c424aef17fd2
Nikola Mihin <nmihin@gmail.com>
2023-11-20 11:09:49 +0100

    feat: updates methods

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,31 +1,56 @@
-import Loader from "@/components/shared/Loader";
+import { Models } from "appwrite";
+
+// import { useToast } from "@/components/ui/use-toast";
+import { Loader, PostCard } from "@/components/shared";
+import { useGetRecentPosts, useGetUsers } from "@/lib/react-query/queriesAndMutations";
 
 const Home = () => {
-  const isPostLoading = true;
-  const posts = null;
+  // const { toast } = useToast();
 
-  const { data:posts, isPending:  isPostLoading, isError: isErrorPosts } = useGetRecentPosts()
+  const {
+    data: posts,
+    isLoading: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+  const {
+    data: creators,
+    isLoading: isUserLoading,
+    isError: isErrorCreators,
+  } = useGetUsers(10);
+
+  if (isErrorPosts || isErrorCreators) {
+    return (
+      <div className="flex flex-1">
+        <div className="home-container">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+        <div className="home-creators">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
-          <h2 className="w-full text-left h3-bold md:h2-bold">
-            Home Feed
-          </h2>
+          <h2 className="w-full text-left h3-bold md:h2-bold">Home Feed</h2>
           {isPostLoading && !posts ? (
             <Loader />
-          ): (
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+          ) : (
+            <ul className="flex flex-col flex-1 w-full gap-9 ">
+              {posts?.documents.map((post: Models.Document) => (
+                <li key={post.$id} className="flex justify-center w-full">
+                  <PostCard post={post} />
+                </li>
+              ))}
             </ul>
           )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
