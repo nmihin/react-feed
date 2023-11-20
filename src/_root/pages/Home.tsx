2cddaa99464eacae8cea05ec57a14d653497dff1
Nikola Mihin <nmihin@gmail.com>
2023-11-20 09:07:17 +0100

    feat: home feed

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,30 @@
-
+import Loader from "@/components/shared/Loader";
 
 const Home = () => {
+  const isPostLoading = true;
+  const posts = null;
+
+  const { data:posts, isPending:  isPostLoading, isError: isErrorPosts } = useGetRecentPosts()
+
   return (
-    <div>Home</div>
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="w-full text-left h3-bold md:h2-bold">
+            Home Feed
+          </h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ): (
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
